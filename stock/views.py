8d70e1eb168bda3fdb0e29eadc1d7afa0da39ae7
new <61010923@kmitl.ac.th>
2020-10-07 17:23:18 +0700

    final project

--- stock/views.py
+++ stock/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from .models import Product
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    return render (request, 'frontend/index.html',{'products': products})
+    return render (request, 'frontend/index.html',)
 def about(request):
     return render (request, 'frontend/about.html')
 def contact(request):
